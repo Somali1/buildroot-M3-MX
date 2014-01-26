c476825856eb064402acfba33ac59210478b9b7d
J1nx <admin@j1nx.nl>
2014-01-26 20:52:38 +0000

    [TLBB-M3] Add TLBB skin

---
+++ package/thirdparty/tlbb/tlbb.mk
@@ -0,0 +1,24 @@
+TLBB_VERSION = 41cfadb2f280dddc68dffe8198deddb997dd7e65
+TLBB_SITE_METHOD = git
+TLBB_SITE = https://github.com/Nessus85100/tlbb.git
+TLBB_INSTALL_STAGING = YES
+TLBB_INSTALL_TARGET = YES
+TLBB_DEPENDENCIES = xbmc
+TEXTURE_PACKER=$(XBMC_DIR)/tools/TexturePacker/TexturePacker
+
+define TLBB_BUILD_CMDS
+	$(TEXTURE_PACKER) -use_none -i $(@D)/media -o $(@D)/media/Textures.xbt
+endef
+
+define TLBB_INSTALL_STAGING_CMDS
+        mkdir -p $(STAGING_DIR)/usr/share/xbmc/addons/skin.tlbb
+        cp -rf $(@D)/* $(STAGING_DIR)/usr/share/xbmc/addons/skin.tlbb/
+endef
+
+define TLBB_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb/media
+	cd $(@D); cp -rf `ls -I media -1` $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb
+	cp -f $(@D)/media/Textures.xbt $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb/media
+endef
+
+$(eval $(generic-package))
